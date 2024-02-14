6c0c0e70f38ddb1bc1bcd262984fa462e6e99ef0
Bill <brussell@bishopdubourg.org>
2024-02-13 19:10:52 -0600

    add pid test branch

---
+++ src/main/cpp/Drivetrain.cpp
@@ -0,0 +1,101 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Drivetrain.h"
+
+void Drivetrain::Drive(units::meters_per_second_t xSpeed,
+                       units::meters_per_second_t ySpeed,
+                       units::radians_per_second_t rot, bool fieldRelative,
+                       units::second_t period) {
+  auto states =
+      m_kinematics.ToSwerveModuleStates(frc::ChassisSpeeds::Discretize(
+          fieldRelative ? frc::ChassisSpeeds::FromFieldRelativeSpeeds(
+                              xSpeed, ySpeed, rot, m_gyro.GetRotation2d())
+                        : frc::ChassisSpeeds{xSpeed, ySpeed, rot},
+          period));
+
+  m_kinematics.DesaturateWheelSpeeds(&states, kMaxSpeed);
+
+  auto [fl, fr, bl, br] = states;
+
+  m_frontLeft.SetDesiredState(fl);
+  m_frontRight.SetDesiredState(fr);
+  m_backLeft.SetDesiredState(bl);
+  m_backRight.SetDesiredState(br);
+
+ //m_frontLeft.m_driveEncoder.SetMinRate(3);
+ //m_frontRight.m_driveEncoder.SetMinRate(3);
+ //m_backLeft.m_driveEncoder.SetMinRate(3);
+ //m_backRight.m_driveEncoder.SetMinRate(3);
+ //
+ //m_frontLeft.m_turningEncoder.SetMinRate(3);
+ //m_frontRight.m_turningEncoder.SetMinRate(3);
+ //m_backLeft.m_turningEncoder.SetMinRate(3);
+ //m_backRight.m_turningEncoder.SetMinRate(3);
+
+
+
+    frc::SmartDashboard::PutNumber("Front Left Drive Motor Output (Percent)", m_frontLeft.m_driveMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Front Left Drive Motor Output (Volts)", m_frontLeft.m_driveMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Front Left Turning Motor Output (Percent)", m_frontLeft.m_turningMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Front Left Turning Motor Output (Volts)", m_frontLeft.m_turningMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Front Left Drive Motor Output (Amps)", m_frontLeft.m_driveMotor.GetOutputCurrent());
+    frc::SmartDashboard::PutNumber("Front Left Turning Motor Output (Amps)", m_frontLeft.m_turningMotor.GetOutputCurrent());
+
+    frc::SmartDashboard::PutNumber("Front Right Drive Motor Output (Percent)", m_frontRight.m_driveMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Front Right Drive Motor Output (Volts)", m_frontRight.m_driveMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Front Right Turning Motor Output (Percent)", m_frontRight.m_turningMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Front Right Turning Motor Output (Volts)", m_frontRight.m_turningMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Front Right Drive Motor Output (Amps)", m_frontRight.m_driveMotor.GetOutputCurrent());
+    frc::SmartDashboard::PutNumber("Front Right Turning Motor Output (Amps)", m_frontRight.m_turningMotor.GetOutputCurrent());
+
+    frc::SmartDashboard::PutNumber("Back Left Drive Motor Output (Percent)", m_backLeft.m_driveMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Back Left Drive Motor Output (Volts)", m_backLeft.m_driveMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Back Left Turning Motor Output (Percent)", m_backLeft.m_turningMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Back Left Turning Motor Output (Volts)", m_backLeft.m_turningMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Back Left Drive Motor Output (Amps)", m_backLeft.m_driveMotor.GetOutputCurrent());
+    frc::SmartDashboard::PutNumber("Back Left Turning Motor Output (Amps)", m_backLeft.m_turningMotor.GetOutputCurrent());
+
+    frc::SmartDashboard::PutNumber("Back Right Drive Motor Output (Percent)", m_backRight.m_driveMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Back Right Drive Motor Output (Volts)", m_backRight.m_driveMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Back Right Turning Motor Output (Percent)", m_backRight.m_turningMotor.GetMotorOutputPercent());
+    frc::SmartDashboard::PutNumber("Back Right Turning Motor Output (Volts)", m_backRight.m_turningMotor.GetMotorOutputVoltage());
+    frc::SmartDashboard::PutNumber("Back Right Drive Motor Output (Amps)", m_backRight.m_driveMotor.GetOutputCurrent());
+    frc::SmartDashboard::PutNumber("Back Right Turning Motor Output (Amps)", m_backRight.m_turningMotor.GetOutputCurrent());
+
+
+
+    frc::SmartDashboard::PutNumber("Front Left Drive Encoder Rate", m_frontLeft.m_driveEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Front Left Turning Encoder Rate", m_frontLeft.m_turningEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Front Left Drive Encoder Current Count", m_frontLeft.m_driveEncoder.Get());
+    frc::SmartDashboard::PutNumber("Front Left Turning Encoder Current Count", m_frontLeft.m_turningEncoder.Get());
+
+    frc::SmartDashboard::PutNumber("Front Right Drive Encoder Rate", m_frontRight.m_driveEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Front Right Turning Encoder Rate", m_frontRight.m_turningEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Front Right Drive Encoder Current Count", m_frontRight.m_driveEncoder.Get());
+    frc::SmartDashboard::PutNumber("Front Right Turning Encoder Current Count", m_frontRight.m_turningEncoder.Get());
+
+    frc::SmartDashboard::PutNumber("Back Left Drive Encoder Rate", m_backLeft.m_driveEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Back Left Turning Encoder Rate", m_backLeft.m_turningEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Back Left Drive Encoder Current Count", m_backLeft.m_driveEncoder.Get());
+    frc::SmartDashboard::PutNumber("Back Left Turning Encoder Current Count", m_backLeft.m_turningEncoder.Get());
+
+    frc::SmartDashboard::PutNumber("Back Right Drive Encoder Rate", m_backRight.m_driveEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Back Right Turning Encoder Rate", m_backRight.m_turningEncoder.GetRate());
+    frc::SmartDashboard::PutNumber("Back Right Drive Encoder Current Count", m_backRight.m_driveEncoder.Get());
+    frc::SmartDashboard::PutNumber("Back Right Turning Encoder Current Count", m_backRight.m_turningEncoder.Get());
+
+    frc::SmartDashboard::PutNumber("gyro get angle", m_gyro.GetAngle());
+    frc::SmartDashboard::PutNumber("gyro get rate", m_gyro.GetRate());
+
+
+
+
+}
+
+void Drivetrain::UpdateOdometry() {
+  m_odometry.Update(m_gyro.GetRotation2d(),
+                    {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+                     m_backLeft.GetPosition(), m_backRight.GetPosition()});
+}
---
+++ src/main/include/Drivetrain.h
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <numbers>
+
+#include <frc/ADXRS450_Gyro.h>
+#include <frc/geometry/Translation2d.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+#include "SwerveModule.h"
+
+/**
+ * Represents a swerve drive style drivetrain.
+ */
+class Drivetrain {
+ public:
+  Drivetrain() { m_gyro.Reset(); }
+
+  void Drive(units::meters_per_second_t xSpeed,
+             units::meters_per_second_t ySpeed, units::radians_per_second_t rot,
+             bool fieldRelative, units::second_t period);
+  void UpdateOdometry();
+
+  static constexpr units::meters_per_second_t kMaxSpeed =
+      3.0_mps;  // 3 meters per second
+  static constexpr units::radians_per_second_t kMaxAngularSpeed{
+      std::numbers::pi};  // 1/2 rotation per second
+
+ private:
+  frc::Translation2d m_frontLeftLocation{+0.381_m, +0.381_m};
+  frc::Translation2d m_frontRightLocation{+0.381_m, -0.381_m};
+  frc::Translation2d m_backLeftLocation{-0.381_m, +0.381_m};
+  frc::Translation2d m_backRightLocation{-0.381_m, -0.381_m};
+
+/*(int driveMotorChannel, int turningMotorChannel, 
+int driveEncoderChannelA, int driveEncoderChannelB, 
+int turningEncoderChannelA, int turningEncoderChannelB);*/
+
+  SwerveModule m_frontLeft{4, 8, 0, 1, 2, 3};
+  SwerveModule m_frontRight{3, 7, 8, 9, 10, 11};
+  SwerveModule m_backLeft{2, 6, 4, 5, 6, 7};
+  SwerveModule m_backRight{1, 5, 12, 13, 14, 15};
+
+  frc::ADXRS450_Gyro m_gyro;
+
+  frc::SwerveDriveKinematics<4> m_kinematics{
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation,
+      m_backRightLocation};
+
+  frc::SwerveDriveOdometry<4> m_odometry{
+      m_kinematics,
+      m_gyro.GetRotation2d(),
+      {m_frontLeft.GetPosition(), m_frontRight.GetPosition(),
+       m_backLeft.GetPosition(), m_backRight.GetPosition()}};
+};
